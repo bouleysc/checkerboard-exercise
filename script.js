0b544d82ff57af1134a9fe2ecf223a352d5b7371
Sally Bouley <sallybouley87@gmail.com>
2017-06-12 16:29:43 -0600

    black and red

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-// Your JS goes here
+
+for (var i=1; i<82;i++) {
+  var div = document.createElement('div');
+  document.body.appendChild(div);
+  div.style.width = "11.1%";
+  div.style.paddingBottom = "11.1%";
+  div.style.float = "left";
+  if(i%2){
+    div.style.backgroundColor = "black";
+  } else {
+    div.style.backgroundColor = "red";
+  }
+}
