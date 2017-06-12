1e3bb76f273d03e017c0f2e6924c0aa69b5dfd3f
Sally Bouley <sallybouley87@gmail.com>
2017-06-12 17:45:27 -0600

    gradient

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ for (var i=1; i<82;i++) {
   div.style.width = "11.1%";
   div.style.paddingBottom = "11.1%";
   div.style.float = "left";
-  if(i%2){
-    div.style.backgroundColor = "black";
+  if (i%2) {
+    div.style.backgroundColor = "#00f";
+    div.style.opacity = "0.3"
   } else {
-    div.style.backgroundColor = "red";
+    div.style.backgroundColor = "#3fe";
+    div.style.opacity = "0.3"
   }
 }
+var body = document.getElementsByTagName('body')[0];
+body.style.background = "linear-gradient(to bottom, #3fe, #00f)";
+body.style.boxSizing = "border-box";
+body.style.margin = "0";
