f339260b0084d324a493ad490d69052c168d13b7
Sally Bouley <sallybouley87@gmail.com>
2017-06-12 16:55:41 -0600

    random colors

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+  color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+ }
 for (var i=1; i<82;i++) {
   var div = document.createElement('div');
   document.body.appendChild(div);
   div.style.width = "11.1%";
   div.style.paddingBottom = "11.1%";
   div.style.float = "left";
-  if(i%2){
-    div.style.backgroundColor = "black";
-  } else {
-    div.style.backgroundColor = "red";
-  }
+  div.style.backgroundColor = getRandomColor();
 }
