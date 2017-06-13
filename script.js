f641395b52b1afd65730e9460b6bf01a0ac9242a
Sally Bouley <sallybouley87@gmail.com>
2017-06-12 20:37:41 -0600

    almost there

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-
-for (var i=1; i<82;i++) {
-  var div = document.createElement('div');
-  document.body.appendChild(div);
-  div.style.width = "11.1%";
-  div.style.paddingBottom = "11.1%";
-  div.style.float = "left";
-  if(i%2){
-    div.style.backgroundColor = "black";
-  } else {
-    div.style.backgroundColor = "red";
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function checkboard () {
+  for (var i=1; i<82;i++) {
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.style.width = "11.1%";
+    div.style.paddingBottom = "11.1%";
+    div.style.float = "left";
   }
 }
+console.log(div);
+// function randomColorCheckboard() {
+//   var div = checkboard();
+//   for (var i = 0; i < div; i++) {
+//     div.style.background = getRandomColor();
+//   }
+// }
+// setInterval(randomColorCheckboard, 2000);
